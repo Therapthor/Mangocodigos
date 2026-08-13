8ed13433647102a20c8f14fc1b79ce0dbcb55733
Therapthor <gusmelendezlopez@gmail.com>
2026-08-13 16:07:32 -0500

    Add CORS middleware to server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
